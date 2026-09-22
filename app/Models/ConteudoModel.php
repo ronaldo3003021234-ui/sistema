8fe36551882caa23cf20777f424755f0296e2aef
Ronaldo da Silva PIres <ronaldo3003021234@gmail.com>
2026-09-22 20:40:22 -0300

    Fix allowedFields array syntax in ConteudoModel

--- app/Models/ConteudoModel.php
+++ app/Models/ConteudoModel.php
@@ -12,7 +12,7 @@ class ConteudoModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['titulo,'];
+    protected $allowedFields    = ['titulo'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
